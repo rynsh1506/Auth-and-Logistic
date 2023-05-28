1ee67d2257fa811127a9159e0eedcac57ce7d9de
unknown <mriansyah112@gmail.com>
2023-05-28 01:39:09 -0800

    Menambahkan fitur baru untuk menambahkan data logistik

--- controllers/logisticController.js
+++ controllers/logisticController.js
@@ -1,13 +1,32 @@
 const Logistic = require("../models/logistic");
-const { $where } = require("../models/user");
+
+const addLogisticData = async (req, res, next) => {
+    try {
+        const {
+            logistic_name,
+            amount,
+            destination_name,
+            origin_name,
+            duration,
+        } = req.body;
+
+        const newLogistic = new Logistic({
+            logistic_name,
+            amount,
+            destination_name,
+            origin_name,
+            duration,
+        });
+
+        await newLogistic.save();
+        res.status(200).json({ message: "Logistic data added successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
 
 const fetchRates = async (req, res) => {
     try {
-        // Check if user is authenticated (valid JWT)
-        if (!req.user) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-
         // Fetch logistic rates from database
         const rates = await Logistic.find();
         res.status(200).json({ rates });
@@ -18,10 +37,6 @@ const fetchRates = async (req, res) => {
 
 const getLogisticData = async (req, res) => {
     try {
-        if (!req.user) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-
         const { origin_name, destination_name, duration } = req.query;
 
         // Temukan data logistik berdasarkan nama asal dan tujuan
@@ -41,4 +56,4 @@ const getLogisticData = async (req, res) => {
     }
 };
 
-module.exports = { fetchRates, getLogisticData };
+module.exports = { fetchRates, getLogisticData, addLogisticData };
--- routes/logisticRoutes.js
+++ routes/logisticRoutes.js
@@ -6,5 +6,6 @@ const isAuthenticated = require("../middleware/authMiddleware");
 
 router.get("/rates", isAuthenticated, logisticController.fetchRates);
 router.get("/data", isAuthenticated, logisticController.getLogisticData);
+router.post("/add", isAuthenticated, logisticController.addLogisticData);
 
 module.exports = router;
